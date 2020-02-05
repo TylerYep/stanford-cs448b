dbd7f4a8a55229a9e6cbed16dbf61f5e2a84b393
Joy Yuzuriha <yuzurjoy@stanford.edu>
2020-02-04 23:52:42 -0800

    Max circles and restaurants in circle intersection

--- proj3/viz.js
+++ proj3/viz.js
@@ -2,6 +2,7 @@ function main() {
     const PLOT_WIDTH = 1000;
     const PLOT_HEIGHT = 800;
     const CIRCLE_RADIUS = 100;
+    const MAX_CIRCLES = 2;
     let restaurantData;
     const svg = d3.select('#vis')
                 .attr('width', PLOT_WIDTH)
@@ -20,13 +21,28 @@ function main() {
         };
     }).then(data => {
         restaurantData = data;
-        drawPoints(svg, data, projection, null, null, null);
+        drawPoints(svg, data, projection, null, null);
 
+        let numCircles = 0;
+        let circleOneX = null;
+        let circleOneY = null;
+        let circleTwoX = null;
+        let circleTwoY = null;
         svg.on('click', function() {
-            var coords = d3.mouse(this);
-            console.log(coords);
-            drawCircle(svg, coords[0], coords[1], CIRCLE_RADIUS);
-            drawPoints(svg, restaurantData, projection, coords[0], coords[1], CIRCLE_RADIUS);
+            if (numCircles < MAX_CIRCLES) {
+                var coords = d3.mouse(this);
+                console.log(coords);
+                if (numCircles == 0) {
+                    circleOneX = coords[0];
+                    circleOneY = coords[1];
+                } else {
+                    circleTwoX = coords[0];
+                    circleTwoY = coords[1];
+                }
+                drawCircle(svg, coords[0], coords[1], CIRCLE_RADIUS);
+                drawPoints(svg, restaurantData, projection, [circleOneX, circleOneY, CIRCLE_RADIUS], [circleTwoX, circleTwoY, CIRCLE_RADIUS]);
+            }
+            numCircles++;
         })
     });
 }
@@ -37,7 +53,7 @@ function main() {
 //     drawPoints(newData);
 // });
 
-function drawPoints(svg, restaurants, projection, circle_x, circle_y, radius) {
+function drawPoints(svg, restaurants, projection, circleOne, circleTwo) {
     const DOTSIZE = 3;
     let pointsData = svg.selectAll('points').data(restaurants, d => d.name).enter();
     // pointsData.filter(function(d) {
@@ -51,11 +67,18 @@ function drawPoints(svg, restaurants, projection, circle_x, circle_y, radius) {
     // })
     pointsData.append('circle')
             .style('fill', function(d) {
-                if (circle_x != null && circle_y != null) {
-                    var restaurant_x = projection([d.longitude, d.latitude])[0];
-                    var restaurant_y = projection([d.longitude, d.latitude])[1];
-                    var distance_square = Math.pow((restaurant_x - circle_x), 2) + Math.pow((restaurant_y - circle_y), 2);
-                    if (distance_square < Math.pow(radius, 2)) {
+                if (circleOne != null) {
+                    var restaurantX = projection([d.longitude, d.latitude])[0];
+                    var restaurantY = projection([d.longitude, d.latitude])[1];
+                    var distanceSquareOne = Math.pow((restaurantX - circleOne[0]), 2) + Math.pow((restaurantY - circleOne[1]), 2);
+                    if (circleTwo[0] != null) {
+                        var distanceSquareTwo = Math.pow((restaurantX - circleTwo[0]), 2) + Math.pow((restaurantY - circleTwo[1]), 2);
+                        if (distanceSquareOne < Math.pow(circleOne[2], 2) && distanceSquareTwo < Math.pow(circleTwo[2], 2)) {
+                            return 'orange';
+                        }
+                        return 'gray';
+                    }
+                    if (distanceSquareOne < Math.pow(circleOne[2], 2)) {
                         return 'orange';
                     }
                     return 'gray';
