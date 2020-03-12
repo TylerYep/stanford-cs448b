19f234d5b6dd8f5c1b193ba66310d15211425011
Tyler Yep <tyep@stanford.edu>
2020-03-11 19:54:30 -0700

    problem for joy to fix?

--- finalproj/assets/js/timeViz.js
+++ finalproj/assets/js/timeViz.js
@@ -8,7 +8,7 @@ function get_colors(n) {
 
 const MARGIN2 = {top: 61, right: 140, bottom: 101, left: 50},
   WIDTH = 960 - MARGIN2.left - MARGIN2.right,
-  HEIGHT = 450 - MARGIN2.top - MARGIN2.bottom;
+  HEIGHT = 900 - MARGIN2.top - MARGIN2.bottom;
 
 const x = d3.scaleLinear().range([0, WIDTH]);
 const y = d3.scaleLinear().range([HEIGHT, 0]);
@@ -30,37 +30,37 @@ var area = d3.area()
 var stack = d3.stack()
   .value(function(d) { return d.values; });
 
-var svg2 = d3.select("#timeVis").append("svg")
+var svg2 = d3.select("#timeVis")
   .attr("width", WIDTH + MARGIN2.left + MARGIN2.right)
   .attr("height", HEIGHT + MARGIN2.top + MARGIN2.bottom)
-.append("g")
+  .append("g")
   .attr("transform", "translate(" + MARGIN2.left + "," + MARGIN2.top + ")");
 
 d3.csv("data/month_and_severity_counts.csv", d => {
-  return {
-    month: +d.month,
-    severity_1: +d.severity_1,
-    severity_2: +d.severity_2,
-    severity_3: +d.severity_3,
-    severity_4: +d.severity_4
-  };
+    return {
+      month: +d.month,
+      severity_1: +d.severity_1,
+      severity_2: +d.severity_2,
+      severity_3: +d.severity_3,
+      severity_4: +d.severity_4
+    };
   }).then(data => {
-    color.domain(d3.keys(data[0]).filter(function(key) {return key !== "month"; }));
-
-    var browsers = stack(color.domain().map(function(name) {
+    console.log(data);
+    color.domain(d3.keys(data[0]).filter(key => key !== "month"))
+    var browsers = color.domain().map(function(name) {
       return {
         name: name,
         values: data.map(function(d) {
           return {month: d.month, y: d[name] * 1};
         })
       };
-    }));
+    });
+    console.log(browsers)
 
     //   // Find the value of the month with highest total value
     var maxMonthVal = d3.max(data, function(d){
       var vals = d3.keys(d).map(
-      function(key){
-        return key !== "month" ? d[key] : 0 });
+      function(key){ return key !== "month" ? d[key] : 0 });
       return d3.sum(vals);
     });
 
@@ -69,37 +69,37 @@ d3.csv("data/month_and_severity_counts.csv", d => {
     y.domain([0, 60000])
 
     var browser = svg2.selectAll(".browser")
-      .data(browsers)
-        .enter().append("g")
+      .data(browsers).join(
+        enter => enter.append("g")
+      )
       .attr("class", "browser");
 
-    browser.append("path")
+    browser.append("path") // TODO PROBLEM HERE d is not valid value
       .attr("class", "area")
       .attr("d", function(d) { return area(d.values); })
-      .style("fill", function(d,i) {
-          return get_colors(i); });
+      .style("fill", function(d,i) { return get_colors(i); });
 
-    browser.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.month) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
-      .attr("x", -6)
-      .attr("dy", "-0.882em")
-      .text(function(d) {
-          if(d.name == "severity_1") {
-            return "Severity 1";
-          }
-          else if(d.name == "severity_2"){
-            return "Severity 2";
-          }
-          else if(d.name == "severity_3"){
-            return "Severity 3";
-          }
-          else {
-            return "Severity 4";
-          }
-        })
-      .style("font", "15px avenir")
-        .attr("transform", function(d) { return "translate(500," + y(d.value.y0 + d.value.y / 2) + ")"; })
+    // browser.append("text")
+    //   .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+    //   .attr("transform", function(d) { return "translate(" + x(d.value.month) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+    //   .attr("x", -6)
+    //   .attr("dy", "-0.882em")
+    //   .text(function(d) {
+    //       if(d.name == "severity_1") {
+    //         return "Severity 1";
+    //       }
+    //       else if(d.name == "severity_2"){
+    //         return "Severity 2";
+    //       }
+    //       else if(d.name == "severity_3"){
+    //         return "Severity 3";
+    //       }
+    //       else {
+    //         return "Severity 4";
+    //       }
+    //     })
+    //   .style("font", "15px avenir")
+    //     .attr("transform", function(d) { return "translate(500," + y(d.value.y0 + d.value.y / 2) + ")"; })
 
     svg2.append("g")
       .attr("class", "x axis")
@@ -145,7 +145,6 @@ d3.csv("data/month_and_severity_counts.csv", d => {
 
       legend.append("text")
       .attr("x",0)
-    //      .attr("dy", "0.75em")
       .attr("y",-10)
       .text("Categories");
 });
